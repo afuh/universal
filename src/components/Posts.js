448881887b1066020742c248d9a596f128dc438e
Axel <axelfuh@gmail.com>
2017-10-21 19:59:00 +0200

    Add Post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -14,8 +14,64 @@ import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
 import {cyan100, lightBlack} from 'material-ui/styles/colors';
 import Subheader from 'material-ui/Subheader';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 const avatars = ['/images/rick.png', '/images/morty.jpg', '/images/doofus-rick.jpg', '/images/Evilmorty.jpg']
+
+class CreatePost extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      title: '',
+      text: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event){
+    const { id, value } = event.target
+    if (id === "title") {
+      this.setState({ title: value})
+    } else {
+      this.setState({ text: value})
+    }
+  }
+  handleSubmit(){
+    const { title, text } = this.state
+    axios.post('/api/post', { title, text }).then(() => {
+      this.setState({ title: '', text: ''})
+      this.props.addPost()
+    })
+  }
+  render () {
+    const { text, title } = this.state
+    return (
+      <div>
+        <div style={{display: "flex", justifyContent: "space-around"}}>
+          <TextField
+            id="title"
+            floatingLabelText="Title"
+            value={title}
+            onChange={this.handleChange}
+          />
+          <TextField
+            id="text"
+            floatingLabelText="Post"
+            value={text}
+            onChange={this.handleChange}
+          />
+        </div>
+        <FlatButton
+          onClick={this.handleSubmit}
+          style={{margin: "20px 0"}}
+          label="Submit"
+          primary={true}
+          fullWidth={true}/>
+      </div>
+    )
+  }
+}
 
 class Posts extends React.Component {
   constructor(){
@@ -23,12 +79,16 @@ class Posts extends React.Component {
     this.state = {
       posts: []
     }
+    this.handleData = this.handleData.bind(this)
   }
   componentDidMount(){
     axios.get('/api/post').then(res => this.handleState(res.data))
   }
   handleState(data){
     this.setState({posts: data})
+  }
+  handleData(){
+    axios.get('/api/post').then(res => this.handleState(res.data))
   }
   render () {
     const { posts } = this.state
@@ -38,6 +98,9 @@ class Posts extends React.Component {
         <Paper>
           <Subheader>Posts</Subheader>
           <List style={{padding: 0}}>
+            <ListItem containerElement={<CreatePost addPost={this.handleData}/>}
+            />
+            <Divider />
             {posts.map(p => (
             <div key={p._id}>
               <ListItem
